32fe39b85ae352c9711dfa016a6291e8974ec86d
Serena Lew <36708701+minibaguettes@users.noreply.github.com>
2024-03-18 21:07:39 -0700

    added p5r ui + states

--- javascript.js
+++ javascript.js
@@ -1,89 +1,238 @@
-const outcome = document.querySelector('#outcome');
-const buttons = document.querySelectorAll(".btn");
+// heading
 const heading = document.querySelector('#heading');
-heading.classList.add('center');
 
-const losses = document.createElement('div');
-losses.classList.add('score');
-losses.classList.add('score-l');
-heading.appendChild(losses);
+const headingRect = document.querySelector('.heading-rect');
+headingRect.textContent = 'Bringer of Misfortune';
 
-const playBtn = document.createElement('button');
-playBtn.classList.add('header-init');
-heading.appendChild(playBtn);
+//cpu
+const cpu = document.querySelector('#cpu');
 
-const wins = document.createElement('div');
-wins.classList.add('score');
-wins.classList.add('score-w');
-heading.appendChild(wins);
+const cpuIdle = 'p5r/p5r-shiki-ouji-idle.png';
+const cpuWin = 'p5r/p5r-shiki-ouji-attack.png';
 
-const btnr = document.querySelector('#btnr');
+let cpuImg = document.querySelector('#cimg');
+cpuImg.src = cpuIdle;
+
+const cpuScoreImg = document.querySelector('#cpu-score-img');
+cpuScoreImg.src = 'p5r/p5r-cpu-hp-5.png';
+
+let cpuChoice = document.createElement('span');
+let cpuScore = document.createElement('span');
+cpuScore.classList.add('cpuScore');
+cpu.appendChild(cpuChoice);
+cpu.appendChild(cpuScore);
+
+let cpuTemp = cpuScoreImg.src;
+
+
+// user
+const user = document.querySelector('#user');
+
+const jokerIdle = 'p5r/p5r-joker-idle.png';
+const jokerWin = 'p5r/p5r-joker-win.png';
+const jokerHurt = 'p5r/p5r-joker-hurt.png';
+
+let userImg = document.querySelector('#uimg');
+userImg.src = jokerIdle;
+
+const buttons = document.querySelectorAll(".btn");
+
+const userBg = document.querySelector('#user-bg');
+
+/*const btnr = document.querySelector('#btnr');
 const btnp = document.querySelector('#btnp');
-const btns = document.querySelector('#btns');
-const imgChoices = document.querySelector('#img-choices');
+const btns = document.querySelector('#btns');*/
 
-const cpuChoice = document.querySelector('#cpu-choice');
-const cpuIcon = document.createElement('span');
-cpuChoice.appendChild(cpuIcon);
+// footing
+const footing = document.querySelector('#footing');
+const footingScore = document.querySelector('.footing-score');
+const footingScoreHP = document.querySelector('.footing-score-HP');
+
+let userScore = document.createElement('span');
+userScore.classList.add('userScore');
+const userScoreImg = document.querySelector('#user-score-img');
+userScoreImg.src = 'p5r/p5r-user-score.png';
+footingScore.appendChild(userScore);
+
+const dialogue = document.querySelector('#dialogue');
+dialogue.classList.add('hide');
+const dialogueImg = document.createElement('img');
+dialogueImg.src = 'p5r/p5r-dialogue.png';
+dialogueImg.classList.add('dialogue-img');
+const dialogueText = document.createElement('span');
+dialogueText.classList.add('dialogue-text');
+dialogue.appendChild(dialogueImg);
+dialogue.appendChild(dialogueText);
+
+// other
+const scene = document.querySelector('#scene');
+const sceneBtns = document.querySelector('#scene-btns');
+const homeBtn = document.querySelector('#home');
+const playBtn = document.querySelector('#play');
+
+const startScreen = document.createElement('img');
+startScreen.src = 'p5r/p5r-start-screen.png';
+startScreen.classList.add('start-screen');
+
+const loadingScreen = document.createElement('img');
+loadingScreen.src = 'p5r/p5r-general-loading-screen.png';
+loadingScreen.classList.add('loading-screen');
+
+const critScreen = document.createElement('img');
+critScreen.src = 'p5r/p5r-crit.png';
+critScreen.classList.add('crit-screen');
+
+const victoryScreen = document.createElement('img');
+victoryScreen.src = 'p5r/p5r-victory.png';
+victoryScreen.classList.add('victory-screen');
+
+const victoryEnd = document.createElement('img');
+victoryEnd.src = 'p5r/p5r-win-end.png';
+victoryEnd.classList.add('victory-end');
+
+const loseLoad = document.createElement('img');
+loseLoad.src = 'p5r/p5r-velvet-room-loading.png';
+loseLoad.classList.add('lose-load');
+
+const loseEnd = document.createElement('img');
+loseEnd.src = 'p5r/p5r-velvet-room.png';
+loseEnd.classList.add('lose-end');
+
+const loseDialogue = document.createElement('span');
+loseDialogue.textContent = 'You have lost.'
+loseDialogue.classList.add('lose-dialogue');
 
 // *~*~*~*~*~*~*~**~*~*~*~*~*~*~*~*~*~**~*~*~*~*~*~*~*~*~*~**~*~*~*~*~*~ //
 
-resetGame();
-
 playBtn.addEventListener('click', function() {
-  prepareGame();
-  // play game
-  playGame();
+  
+  preGame();
 });
+
+homeBtn.addEventListener('click', function() {
+  homeScreen();
+});
+
+
+let keyChoice = '';
+const handleKeys = (e) => {
+  if (e.key == 'a') {
+    keyChoice = 'rock';
+  }
+  else if (e.key == 's') {
+    keyChoice = 'paper';
+  }
+  else if (e.key == 'd') {
+    keyChoice = 'scissor';
+  }
+  console.log(playRound(keyChoice, getComputerChoice()));
+}
+
+
+
+cpuScoreImg.addEventListener('mouseover', function() {
+  cpuScore.classList.toggle('hp-toggle');
+  cpuScoreImg.src = 'p5r/p5r-cpu-hp-0.png';
+});
+
+cpuScoreImg.addEventListener('mouseout', function() {
+  cpuScore.classList.toggle('hp-toggle');
+  cpuScoreImg.src = cpuTemp;
+});
+
+
+homeScreen();
+//endGame();
 
 // *~*~*~*~*~*~*~**~*~*~*~*~*~*~*~*~*~**~*~*~*~*~*~*~*~*~*~**~*~*~*~*~*~ //
 
 // prepare game
-function prepareGame() {
-  resetGame();
-  // change "start" to "restart game"
-  playBtn.textContent = "Restart Game";
-
-  // show rps choices
-  buttons.forEach(function(button) {
-    button.style.display = "block";
-  });
-
-  // set cpu choice "?"
-  cpuIcon.className = 'fa fa-question-circle';
-  cpuIcon.innerHTML = "";
-  imgChoices.style.display = 'flex';
-
-  wins.classList.add('score-w');
-  losses.classList.add('score-l');
+function preGame() {
+  console.log('start');
+  // change to play screen
+  if (startScreen.parentElement === scene) {
+    scene.removeChild(startScreen);
+  }
+  // reset scores
+  cpuScore.textContent = 1;
+  userScore.textContent = 1;
+  // reset images
+  userImg.classList.add('userImgIdle');
+  cpuImg.classList.add('cpuImgIdle');
+  // remove scene
+  if (loseEnd.parentElement === scene) {
+    scene.removeChild(loseEnd);
+    scene.removeChild(loseLoad);
+    scene.removeChild(loseDialogue);
+    scene.classList.remove('blk-screen');
+  }
+  // fix / hide buttons
+  sceneBtns.id = 'scene-btns-fight';
+  playBtn.classList.add('hide');
+  homeBtn.classList.remove('hide');
+  // play
+  playGame();
 }
 
-// reset game screen
-function resetGame() {
-  playBtn.textContent = "Play!";
-  // hide rps choices
-  imgChoices.style.display = 'none';
-  cpuChoice.style.display = 'none';
-  wins.classList.remove('score-w');
-  losses.classList.remove('score-l');
+// show home screen
+function homeScreen() {
+  console.log('home');
+  // change to home screen
+  scene.appendChild(startScreen);
+  // fix buttons
+  sceneBtns.id = 'scene-btns-home';
+  playBtn.classList.remove('hide');
+  homeBtn.classList.add('hide');
+  playBtn.classList.add('play-home')
 }
 
 // end game after 5 rounds
 function endGame() {
-  console.log("gameover! play again? <btn>");
-  playBtn.textContent = "Play Again?";
-  imgChoices.style.display = 'none';
-  cpuChoice.style.display = 'none';
+  // if user wins
+  if (parseInt(cpuScore.textContent) <= 0) {
+    setTimeout(function() {
+      setTimeout(function() {
+        // *** - show victory end screen
+        scene.appendChild(victoryEnd);
+      }, 1000);
+      // ** - show victory screen
+      victoryScreen.classList.add('ttx0');
+    }, 1000);
+    // * - fade in/out crit visual
+    scene.appendChild(critScreen);
+    scene.appendChild(victoryScreen);
+  }
+  // if user loses
+  else {
+    setTimeout(function() {
+      setTimeout(function () {
+        // *** - show lose end screen
+        scene.appendChild(loseEnd);
+        scene.classList.add('blk-screen');
+        scene.appendChild(loseDialogue);
+        setTimeout(function () {
+          // **** - show buttons
+          sceneBtns.classList.remove('hide');
+          playBtn.classList.remove('play-home')
+          playBtn.classList.remove('hide');
+          sceneBtns.id = 'scene-btns-lose';
+          playBtn.textContent = 'Play again?';
+        }, 2000);
+      }, 1000);
+      // ** - show end loading screen
+      scene.appendChild(loseLoad);
+    }, 1000);
+    // * - wait 1s before loading screen
+  }
 }
 
 // *~*~*~*~*~*~*~**~*~*~*~*~*~*~*~*~*~**~*~*~*~*~*~*~*~*~*~**~*~*~*~*~*~ //
 
 // play a game (5 rounds)
-function playGame() {
-  cpuChoice.style.display = 'block';
-  wins.textContent = 0;
-  losses.textContent = 0;
+function playGame() { 
   let choice;  
+  document.addEventListener('keydown', handleKeys);
+
     buttons.forEach(function(button) {
       button.addEventListener("click", function() {
         if (button.id == 'btnr') {
@@ -95,28 +244,11 @@ function playGame() {
         if (button.id == 'btns') {
           choice = 'scissor';
         }
-
-        hide(button.id);
-        
-        if (parseInt(wins.textContent) + parseInt(losses.textContent) < 1) {
-          let cpuChoice = getComputerChoice();
-          cpuIcon.className = '';
-          if (cpuChoice == 'rock') {
-            cpuIcon.innerHTML = "&#9994";
-            cpuIcon.className = '';
-          }
-          else if (cpuChoice == 'paper') {
-            cpuIcon.innerHTML = "&#9995";
-            cpuIcon.className = '';
-
-          }
-          else {
-            cpuIcon.innerHTML = "&#9996";
-            cpuIcon.className = '';
-
-          }
-          outcome.textContent = playRound(choice, cpuChoice);
-          console.log(wins.textContent, ' + ', losses.textContent, ' = ', parseInt(wins.textContent) + parseInt(losses.textContent));
+        cpuChoice = getComputerChoice();
+        console.log(playRound(choice, cpuChoice));
+        if (parseInt(userScore.textContent) <= 0 || parseInt(cpuScore.textContent) <= 0) {
+          endGame();
+          return;
         }
       });
     });
@@ -125,18 +257,22 @@ function playGame() {
 
 // play one round
 function playRound(playerSelection, computerSelection) {
+  console.log(computerSelection + ' (cpu) vs ' +  playerSelection + ' (user)');
   if ((playerSelection == computerSelection)) {
     computeScore(0);
+    userTied(playerSelection, computerSelection);
     return('Tied!');
   }
   else if ((playerSelection == "rock" && computerSelection == 'scissor') ||
   (playerSelection == "paper" && computerSelection == 'rock') ||
   (playerSelection == "scissor" && computerSelection ==  'paper')) {
     computeScore(1);
+    userWin(playerSelection, computerSelection);
     return('Win!');
   }
   else {
     computeScore(-1);
+    userHurt(playerSelection, computerSelection);
     return('Lose!');
   }
 }
@@ -145,11 +281,15 @@ function playRound(playerSelection, computerSelection) {
 
 function computeScore(result) {
   if (result > 0) {
-    wins.textContent = parseInt(wins.textContent) + 1;
+    cpuScore.textContent = parseInt(cpuScore.textContent) - 1;
   }
   else if (result < 0) {
-    losses.textContent = parseInt(losses.textContent) + 1;
+    userScore.textContent = parseInt(userScore.textContent) - 1;
   }
+  cpuScoreImg.src = 'p5r/p5r-cpu-hp-' + cpuScore.textContent + '.png'
+  cpuTemp = cpuScoreImg.src;
+
+  roundWait();
 }
 
 // randomly generate computer choice
@@ -166,27 +306,65 @@ function getComputerChoice() {
   }
 }
 
-// hide user options and disable buttons for 1 second before conitnuing to next round 
-function hide(c) {
-  buttons.forEach(function(button) {
-    setTimeout(function(){
-      button.disabled = false;
-      button.style.display = "block"; 
-      cpuIcon.className = 'fa fa-question-circle';
-      btnr.classList.add('btn-h');
-      btnp.classList.add('btn-h');
-      btns.classList.add('btn-h');
-      // if end of game, signal end game
-      if (parseInt(wins.textContent) + parseInt(losses.textContent) == 1) {
-        endGame();
-      }
-      }, 2000);
-    if (button.id != c){
-      button.style.display = "none"
-    }
-    button.disabled = true;
-    btnr.classList.remove('btn-h');
-    btnp.classList.remove('btn-h');
-    btns.classList.remove('btn-h');
-  });
+function userTied(playerSelection, computerSelection) {
+  userImg.src = jokerIdle;
+  userImg.classList.remove('userImgWin');
+  userImg.classList.add('userImgIdle');
+
+  cpuImg.src = cpuIdle;
+  cpuImg.classList.remove('cpuImgWin');
+  cpuImg.classList.add('cpuImgIdle');
+
+  dialogueText.textContent = playerSelection + ' is tied with ' + computerSelection + '.';
 }
+
+function userWin(playerSelection, computerSelection){
+  userImg.src = jokerWin;
+  userImg.classList.remove('userImgIdle');
+  userImg.classList.add('userImgWin');
+
+  cpuImg.src = cpuIdle;
+  cpuImg.classList.remove('cpuImgWin');
+  cpuImg.classList.add('cpuImgIdle');
+
+  dialogueText.textContent = playerSelection + ' wins against ' + computerSelection + '!';
+}
+
+function userHurt(playerSelection, computerSelection){
+  userImg.src = jokerHurt;
+  userImg.classList.remove('userImgWin')
+  userImg.classList.add('userImgIdle');
+
+  cpuImg.src = cpuWin;
+  cpuImg.classList.add('cpuImgWin');
+  cpuImg.classList.remove('cpuImgIdle');
+
+  dialogueText.textContent = playerSelection + ' loses against ' + computerSelection + '! I lost..';
+}
+
+function roundWait() {
+  setTimeout(function() {
+    userTied();
+    dialogue.classList.toggle('hide');
+    userBg.classList.remove('hide'); 
+    document.addEventListener('keydown', handleKeys);
+    buttons.forEach(function(button) {
+      button.disabled = false;
+      button.classList.toggle('hide');
+    });
+    playBtn.disabled = false;
+  homeBtn.disabled = false;
+  }, 2000);
+
+  buttons.forEach(function(button) {
+    button.disabled = true;
+    button.classList.toggle('hide');
+  });
+  playBtn.disabled = true;
+  homeBtn.disabled = true;
+  dialogue.classList.remove('hide');
+  userBg.classList.add('hide'); 
+  document.removeEventListener('keydown', handleKeys);
+  sceneBtns.classList.add('hide');
+}
+
